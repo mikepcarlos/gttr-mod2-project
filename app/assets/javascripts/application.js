96dc5bb8c5a4d3d11b1d54a8e17365da2976fa73
github username <github email address>
2018-09-11 16:17:48 -0400

    added css styling for home/login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,21 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+window.onload = function(){
+  var loginModal = document.getElementById("login-modal");
+  var loginBtn = document.getElementById("login-btn");
+  var closeLogin = document.getElementsByClassName("close")[0];
+
+  loginBtn.onclick = function(){
+    loginModal.style.display = "block";
+  }
+  closeLogin.onclick = function(){
+    loginModal.style.display = "none";
+  }
+  window.onclick = function(event){
+    if(event.target == loginModal){
+      loginModal.style.display = "none";
+    }
+  }
+}
